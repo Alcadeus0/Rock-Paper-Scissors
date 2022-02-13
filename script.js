9802bde7c2bca6708c4d8c1769d1be39a80cba67
Aazam Thakur <aazamthakur@gmail.com>
2022-02-14 03:15:00 +0530

    Updated UI and added buttons

--- script.js
+++ script.js
@@ -3,46 +3,66 @@ function computerPlay(){
      let ans = Math.floor(Math.random() * 3);
      return picks[ans];
 }
+const player = document.querySelector('#player')
+const bot = document.querySelector('#bot')
 
-function caps(str) {
-    let cap = str.slice(0,1).toUpperCase();
-    let word = str.slice(1);
-    return (cap + word);
-}
+let playerScore = 0;
+let botScore = 0;
 
-function playRound(playerSelection, computerSelection){
-    const player = caps(playerSelection.toLowerCase());
-    if (player === computerSelection) {
-        console.log("It is a tie");
-        return (playRound(prompt("Enter your pick again!"), computerPlay()));
-    }
-    else if ((player === "Rock" && computerSelection === "Scissors") || (player === "Paper" && computerSelection === "Rock") || (player === "Scissors" && computerSelection === "Paper")) {
-        console.log(`You Win! ${player} beats ${computerSelection}`);
-        return 1;
+function playRound(playerSelection){
+    computerSelection = computerPlay();
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        playerScore += 1;
+        player.textContent = playerScore;
         }
     else {
-        console.log(`You Lose! ${computerSelection} beats ${player}`);
-        return -1;
+        botScore += 1;
+        bot.textContent = botScore;
+    }
+    if (playerScore === 5 || botScore === 5){
+        result();
     }
 }
 
-function game(){
-    let input;
-    let playerCount = 0;
-    let computerCount = 0;
-    for (i = 0; i < 5; i++){
-        input = prompt("Enter your pick: ");
-        if (playRound(input, computerPlay()) > 0) {
-            playerCount += 1;
-        }
-        else {
-            computerCount += 1;
-        }
-        }
-    if (playerCount > computerCount) {
-        console.log("You are the winner!");
+function result() {
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+    const div = document.createElement('div');
+    if (playerScore === 5){
+        div.textContent = "You Win!";
     }
-    else{
-        console.log("Computer is the winner!");
+    else {
+        div.textContent = "You Lose!";
     }
+    div.setAttribute('style','color: coral','fontSize: 25px');
+    const result = document.querySelector('.result');
+    result.appendChild(div);
+    refresh()
 }
+
+function refresh(){
+    const btn = document.createElement('button');
+    btn.classList.add('buttons');
+    btn.textContent = "Try Again?"
+    btn.addEventListener('click', () =>{
+        playerScore = 0;
+        botScore = 0;
+        player.textContent = 0;
+        bot.textContent = 0;
+        div.remove();
+        btn.remove();
+        document.getElementById("myBtn").disabled = false;
+        document.getElementById("myBtn").disabled = false;
+        document.getElementById("myBtn").disabled = false;
+    });
+    const result = document.querySelector('.result');
+    result.append(btn);
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        playRound(button.id);
+    });
+});
